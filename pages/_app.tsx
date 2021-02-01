be4f4c49c4de95673c77635390ab7c1335e88520
Max Nelson <m.nelson.study@gmail.com>
2021-02-01 12:31:01 -0800

    React-query example implementation

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,7 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { makeClient } from "@spree/storefront-api-v2-sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+const client = makeClient({
+  host: "https://localhost:8080",
+});
+
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  const products = client.products.list({ page: 1 });
+  console.log("products: ", products);
   res.statusCode = 200;
   res.json({ name: "John Doe" });
 };
