67860d78ee3f82d6d917b2894c19a8d99dfe425c
Max Nelson <m.nelson.study@gmail.com>
2021-03-04 13:59:24 -0800

    Take header off of client-only page

--- pages/client-only.tsx
+++ pages/client-only.tsx
@@ -4,7 +4,6 @@ import { Layout, Header, InfoBox, PostList } from "../components";
 const ClientOnly = () => {
   return (
     <Layout>
-      <Header />
       <InfoBox>ℹ️ This data is loaded on client and not prefetched</InfoBox>
       <PostList />
     </Layout>
