d82e8805e7e8fed632504f8ab68cfcec9313ce42
jiansenhuang <hjszion@gmail.com>
2021-05-16 11:03:02 -0700

    update

--- next.config.js
+++ next.config.js
@@ -38,6 +38,7 @@ const DEPLOY_ENV_MAPPING={
 const envFile=path.join(__dirname,`.env.DNA.${DEPLOY_ENV_MAPPING[DEPLOY_ENV]}`)
 loadEnvVariables();
 const isLocalDevEnvironment = !process.env.DEPLOY_ENV;
+console.log('process.env.TEST',process.env.TEST)
 module.exports = {
   webpack: (config,{webpack}) => {
     config.plugins = config.plugins || [];
