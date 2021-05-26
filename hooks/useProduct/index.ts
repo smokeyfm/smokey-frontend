060877f9b6ef5f7fcd90ef233dfc938aab406b84
Max Nelson <m.nelson.study@gmail.com>
2021-05-26 15:40:15 -0700

    Fix type error and refreshing product details page

--- hooks/useProduct/index.ts
+++ hooks/useProduct/index.ts
@@ -1,25 +1,23 @@
 import { useQuery } from "react-query";
-
 import { IProduct } from "@spree/storefront-api-v2-sdk/types/interfaces/Product";
 import { spreeClient } from "../../config/spree";
-import { SpreeSDKError } from "@spree/storefront-api-v2-sdk/types/errors";
 
-const fetchProduct = async (id: string): Promise<IProduct | SpreeSDKError> => {
+const fetchProduct = async (id: string): Promise<IProduct> => {
   const response = await spreeClient.products.show(id, {
     include: "images",
   });
-
-  if (response.isSuccess) {
+  console.warn("FETCH PRODUCTS");
+  if (response.isSuccess()) {
+    console.warn("FETCH PRODUCTS SUCCESS");
     return response.success();
   } else {
-    return response.fail();
+    console.warn("FETCH PRODUCTS FAILED");
+    throw new Error("Product request failed");
   }
 };
 
 const useProduct = (id: string) => {
-  return useQuery<IProduct | SpreeSDKError, false>(["product", id], () =>
-    fetchProduct(id)
-  );
+  return useQuery<IProduct, false>(["product", id], () => fetchProduct(id));
 };
 
 export { useProduct, fetchProduct };
