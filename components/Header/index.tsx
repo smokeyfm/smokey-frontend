382b9947ce8114f84744cc150fa78c7d6e32badb
Max Nelson <m.nelson.study@gmail.com>
2021-05-26 16:25:04 -0700

    Add precommit checks for typescript, eslint, and prettier

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRouter } from "next/router";
+import { useRouter, useRouter } from "next/router";
 import Link from "next/link";
 
 export const Header = () => {
