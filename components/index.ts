c80e6bcb2ad46c92380ac0b77abdf340e4f3784f
Amadi Someswaran <amadi@someswaran.com>
2021-06-01 14:12:47 -0400

    cleanup: removed header and QueryDevTools from view

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ export * from "./Layout";
 export * from "./Header";
 export * from "./PostList";
 export * from "./ProductList";
+export * from "./ComingSoon";
 export * from "./NewsletterForm"
 export * from "./SocialLinks"
