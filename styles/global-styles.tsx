c85028542803103c884d95229409f57b5478100d
Amadi Someswaran <amadi@someswaran.com>
2021-06-16 16:36:11 -0400

    added beeper logo

--- styles/global-styles.tsx
+++ styles/global-styles.tsx
@@ -7,7 +7,6 @@ export const GlobalStyles = () => (
       styles={css`
         body {
           background: url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80') ;
-          mix-blend-mode: overlay;
           background-size: cover;
           height: 944px;
         }
