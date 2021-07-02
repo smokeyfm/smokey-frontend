57bff91d4bf0c1afac2b6b1f7b258f09eed0c44f
Aaron Smulktis <aaronsmulktis@gmail.com>
2021-07-02 14:28:39 -0400

    cleanup comment and run linter

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -150,7 +150,6 @@ const SearchBar = ({
               </StyledInputPostfix>
             )}
           </StyledInputContainer>
-          {/* <SearchButton>Go</SearchButton> */}
         </SearchInputWrapper>
         <ButtonWrapper>
           <BrowseButton small>Search</BrowseButton>
