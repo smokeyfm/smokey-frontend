aed036492ed0c8111640b37bbd0abd1b0d7bd2af
Aaron Smulktis <aaronsmulktis@gmail.com>
2021-07-03 00:24:11 -0400

    add type for emotion theme, fix a bunch of TS errs

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,23 +1,23 @@
-import '@emotion/react'
+import "@emotion/react";
 
-declare module '@emotion/react' {
+declare module "@emotion/react" {
   export interface Theme {
     colors: {
       blue: {
         primary: string;
         medium: string;
-      },
+      };
       pink: {
         primary: string;
-      },
+      };
       white: {
         primary: string;
-      },
+      };
       gray: {
         primary: string;
         medium: string;
         background: string;
-      }
-    }
+      };
+    };
   }
 }
