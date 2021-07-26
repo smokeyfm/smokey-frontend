5416dae3a01a991a3d574f815e01435a460fd2a5
Max Nelson <m.nelson.study@gmail.com>
2021-07-26 07:44:04 -0700

    Enable showing cart contents and adding items to cart

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { HeaderProps } from "./types";
-import Sticky from "react-sticky-el";
 import { useAuth } from "../../config/auth";
 import SearchBar from "../SearchBar";
 import styled from "@emotion/styled";
+import { Cart } from "../Cart/Cart";
 
 const TopHeader = styled.div`
   padding: 30px 0px;
@@ -44,8 +44,7 @@ const BottomHeader = styled.div`
   align-items: center;
   justify-content: space-between;
   flex-wrap: wrap;
-  padding: 30px 0px;
-  background: black;
+  padding: 5px 0px;
   & > :first-child {
     padding-left: 0px;
   }
@@ -61,77 +60,42 @@ const dummyCategories = ["Best Sellers", "Latest", "Seasonal", "Luxury", "On Sal
 export const Header: React.FC<HeaderProps> = (props) => {
   const { pathname } = useRouter();
   const { user, logout } = useAuth();
+  const [cartVisible, setCartVisible] = React.useState(false);
+  const toggleCart = () => setCartVisible((isVisible) => !isVisible);
 
   return (
     <HeaderDiv>
       <TopHeader>
-        <Link href="/">
-          <LinkDiv isActive={pathname === "/"}>Home</LinkDiv>
-        </Link>
+        <div />
         <LogoDiv>
           <Link href="/">
             <LinkDiv isActive>LOGO</LinkDiv>
           </Link>
         </LogoDiv>
-        <>
-          {user ? (
-            <RightSide>
-              <div>{user.data.attributes.email}</div>
-              <button onClick={logout}>LOGOUT</button>
-            </RightSide>
-          ) : (
-            <RightSide>
-              <Link href="/authenticate/login">
-                <LinkDiv isActive={pathname === "/authenticate/login"}>LOG IN</LinkDiv>
-              </Link>
-              <Link href="/authenticate/signup">
-                <LinkDiv isActive={pathname === "/authenticate/signup"}>SIGN UP</LinkDiv>
-              </Link>
-            </RightSide>
-          )}
-        </>
+        <div>
+          <RightSide>
+            {user ? (
+              <>
+                <div>{user.data.attributes.email}</div>
+                <button onClick={logout}>LOGOUT</button>
+              </>
+            ) : (
+              <>
+                <Link href="/authenticate/login">
+                  <LinkDiv isActive={pathname === "/authenticate/login"}>LOG IN</LinkDiv>
+                </Link>
+                <Link href="/authenticate/signup">
+                  <LinkDiv isActive={pathname === "/authenticate/signup"}>SIGN UP</LinkDiv>
+                </Link>
+              </>
+            )}
+            <Cart isVisible={cartVisible} toggle={toggleCart} />
+          </RightSide>
+        </div>
       </TopHeader>
-      <Sticky>
-        <BottomHeader>
-          {dummyCategories.map((category) => (
-            <Link href="/" key={category}>
-              <Category>{category}</Category>
-            </Link>
-          ))}
-          <style jsx>{`
-            .bottom-header {
-              display: flex;
-              flex-direction: row;
-              align-items: center;
-              justify-content: space-between;
-              flex-wrap: wrap;
-              padding: 30px 0px;
-              background: black;
-            }
-            .category {
-              padding: 15px;
-            }
-            .bottom-header > :first-child {
-              padding-left: 0px;
-            }
-            .bottom-header > :last-child {
-              padding-right: 0px;
-            }
-
-            header {
-              margin-bottom: 25px;
-            }
-            a {
-              font-size: 14px;
-              text-decoration: none;
-            }
-            .is-active {
-              text-decoration: underline;
-            }
-          `}</style>
-          <SearchBar />
-        </BottomHeader>
-      </Sticky>
+      <BottomHeader>
+        <SearchBar />
+      </BottomHeader>
     </HeaderDiv>
   );
 };
