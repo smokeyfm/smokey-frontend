7f0d730282191ae8aff74f1b5f789a52db218de1
Aaron Smulktis <aaronsmulktis@gmail.com>
2021-08-22 15:28:36 -0700

    add line break

--- components/MainMenu/data/menusData.tsx
+++ components/MainMenu/data/menusData.tsx
@@ -3,6 +3,7 @@ import AssignmentIcon from "@material-ui/icons/Assignment";
 import LaptopMacIcon from "@material-ui/icons/LaptopMac";
 import { menuDataItem } from "../types";
 import styled from "@emotion/styled";
+
 export const NewDigWrap = styled.div`
   display: flex;
   background-color: #fff;
