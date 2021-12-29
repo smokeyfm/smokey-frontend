0b0b8462708dc8abff6ec98bd54e043fa1af50f1
Aaron Smulktis <aaronsmulktis@gmail.com>
2021-12-29 00:58:58 -0800

    get mobile menu dynamic

---
+++ components/MainMenu/MainMenu.styles.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const HiddenOnDesktop = styled.div`
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const HiddenOnMobile = styled.div`
+  position: relative;
+  z-index: 3;
+  box-shadow: 0 6px 12px rgb(0 0 0 / 5%);
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+  @media screen and (min-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const MenuToggle = styled.div`
+  position: fixed;
+  width: 36px;
+  height: 30px;
+  left: 1.06vw;
+  top: 3.73vw;
+`;
+
+export const MenuFooter = styled.div`
+  /* position: fixed; */
+  bottom: 0;
+`;
---
+++ components/MainMenu/MobileMenu.tsx
@@ -0,0 +1,137 @@
+import React, { Fragment, useState, useCallback } from 'react';
+import styled from '@emotion/styled';
+import isPropValid from "@emotion/is-prop-valid";
+import { slide as BurgerMenu } from "react-burger-menu";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import Collapse from "@material-ui/core/Collapse";
+import { menuStyles } from "./menuStyles";
+
+import {
+  MenuToggle,
+  MenuFooter
+} from './MainMenu.styles';
+
+export interface MenuItemProps {
+  paddingLeft: string;
+}
+
+const MenuItem = styled(ListItem, {
+  shouldForwardProp: (prop) => isPropValid(prop) && prop !== "paddingLeft"
+})<MenuItemProps>`
+  padding-left: ${(props) => props.paddingLeft}!important;
+`;
+
+export const MobileMenu = ({ showMenuHeader, onMenuItemClick, menusData }: any) => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => setOpen((value: any) => !value);
+
+  const [keyPath, setKeyPath] = useState("");
+  const handleClick = useCallback((kp: any, key: any) => {
+    if (onMenuItemClick) {
+      onMenuItemClick(kp, key);
+    }
+    setKeyPath((pre) => {
+      if (kp == pre) {
+        console.log("closing");
+        let str = kp.replace("/" + key, "");
+        return str;
+      } else {
+        console.log("opening");
+        return kp;
+      }
+    });
+  }, [onMenuItemClick]);
+
+  const getSubMenuItem = (localMenuData: any[], parentKeyPath: string, level: number) => {
+    const paddingLeft = level * 20 + "px";
+    const isArray = Array.isArray(localMenuData);
+    console.log(isArray, localMenuData);
+    const menuItems = isArray ? localMenuData : menusData?.menu_location_listing[0].menu_item_listing;
+    // console.log('local menu: ', localMenuData);
+    return (
+      <List disablePadding>
+        {menuItems.map((item: any, index: any) => {
+          const subItems = item.childrens;
+          const slug = item.name.toLowerCase();
+          const pathSlug = parentKeyPath + "/" + slug;
+          // console.log(subItems, pathSlug, keyPath.indexOf(pathSlug) != -1);
+
+          return (
+            <Fragment key={pathSlug}>
+              {
+                <MenuItem
+                  paddingLeft={paddingLeft}
+                  onClick={handleClick.bind(null, pathSlug, slug)}
+                  button
+                >
+                  {/* <ListItemIcon>{item.icon ? item.icon() : null}</ListItemIcon> */}
+                  {/* <ListItemText primary={item.name.replace("/", "_")} /> */}
+                  <ListItemText primary={item.name} />
+                  {item &&
+                    subItems &&
+                    subItems.length != 0 &&
+                    (keyPath.indexOf(pathSlug) != -1 ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    ))}
+                </MenuItem>
+              }
+              {item && subItems && subItems.length != 0 && (
+                <Collapse
+                  timeout="auto"
+                  unmountOnExit
+                  in={keyPath.indexOf(pathSlug) != -1}
+                >
+                  {level < 2 ? getSubMenuItem(subItems, pathSlug, level + 1) : null}
+                  {/* <h1>hey</h1> */}
+                  {subItems.map(({ item, i }: any) => {
+                    return (
+                      <>{item}</>
+                    )
+                  })}
+                </Collapse>
+              )}
+            </Fragment>
+          );
+        })}
+      </List>
+    );
+  };
+
+  return (
+    <BurgerMenu
+      width={"66%"}
+      isOpen={open}
+      onOpen={toggleMenu}
+      onClose={toggleMenu}
+      styles={menuStyles}
+    // {...others}
+    >
+      {/* <BurgerMenu width={220} isOpen={open} onOpen={toggleMenu} onClose={toggleMenu} {...others}> */}
+      {showMenuHeader ? (
+        <>
+          <div onClick={toggleMenu}>
+            <i className="btb bt-close" />
+          </div>
+        </>
+      ) : null}
+      {/* {getSubMenuItem(menuItemsData && menuItemsData?.response_data.menu_location_listing[0], "", 0)} */}
+      {getSubMenuItem(menusData, "", 0)}
+      <MenuItem
+        paddingLeft={"10px"}
+        onClick={() => console.log('login')}
+        button
+      >Login</MenuItem>
+      <MenuFooter>
+        <div>Privacy Policy - Terms &amp; Conditions - RETURN POLICY</div>
+        <div>All Materials Copyright © 2021 POL Clothing</div>
+      </MenuFooter>
+    </BurgerMenu>
+  )
+};
