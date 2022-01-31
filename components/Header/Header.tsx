29fcf827320de10141f1c197d5827c0e1c0155ad
Aaron Smulktis <aaronsmulktis@gmail.com>
2022-01-31 00:53:58 -0800

    linter fixes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -41,7 +41,7 @@ export const Header: React.FC<HeaderProps> = (props) => {
   const toggleCart = () => setCartVisible((isVisible) => !isVisible);
 
   const isMaint = process.env.IS_MAINT_MODE;
-  const darkMode = (process.env.IS_DARK_MODE === "true");
+  const darkMode = process.env.IS_DARK_MODE === "true";
 
   const handleAccount = (event: any) => {
     setAccountElem(event.currentTarget);
