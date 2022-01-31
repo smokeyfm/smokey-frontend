20ce709f3f3cf809fcaa9c6057b44041e124c577
Aaron Smulktis <aaronsmulktis@gmail.com>
2022-01-31 01:38:44 -0800

    linter

--- components/Header/Header.styles.tsx
+++ components/Header/Header.styles.tsx
@@ -35,7 +35,7 @@ export const LinkDiv = styled.a<LinkDivProps>`
   font-size: 14px;
   text-decoration: none;
   &:hover {
-    color: ${p => p.theme.colors.brand.primary};
+    color: ${(p) => p.theme.colors.brand.primary};
   }
 `;
 
