974efe1d74738f7a19e31427e02d2b7165f1220e
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-08-05 19:57:06 -0400

    Changes: - add send_page_view to GTag init - add dynamic endpoint for serving font files

--- pages/api/fonts/[font].tsx
+++ pages/api/fonts/[font].tsx
@@ -1,14 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const fontPath = path.join(process.cwd(), `../public/fonts/${req.query.file}.woff2`); 
+  const fontPath = path.join(
+    process.cwd(),
+    `../public/fonts/${req.query.file}.woff2`
+  );
   const fontData = fs.readFileSync(fontPath);
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Content-Type', 'font/woff2');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Content-Type", "font/woff2");
 
   res.write(fontData);
 
