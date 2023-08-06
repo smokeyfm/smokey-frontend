f353f1e428db9d99db95b20a8574332f3d226d80
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-08-06 18:38:18 -0400

    linter fix

--- components/LegalLinks/LegalLinks.tsx
+++ components/LegalLinks/LegalLinks.tsx
@@ -8,27 +8,27 @@ import {
 } from "./LegalLinks.styles";
 
 export const LegalLinks = ({ darkMode, hasBackground }: any) => {
-    const currentYear = new Date().getFullYear();
-    return (
-        <LegalContainer darkMode={darkMode} hasBackground={hasBackground}>
-        <LegalRow>
-            All materials copyright © {currentYear}, Material Instinct LLC
-        </LegalRow>
-        <LegalRow>
-            <LegalColumn>
-            <LegalLink
-                darkMode={darkMode}
-                href={process.env.NEXT_PUBLIC_LEGAL_SLUG}
-            >
-                Privacy Policy
-            </LegalLink>
-            </LegalColumn>
-            <LegalColumn>
-            <LegalLink darkMode={darkMode} href="/terms-of-service">
-                Terms of Service
-            </LegalLink>
-            </LegalColumn>
-        </LegalRow>
-        </LegalContainer>
-    );
+  const currentYear = new Date().getFullYear();
+  return (
+    <LegalContainer darkMode={darkMode} hasBackground={hasBackground}>
+      <LegalRow>
+        All materials copyright © {currentYear}, Material Instinct LLC
+      </LegalRow>
+      <LegalRow>
+        <LegalColumn>
+          <LegalLink
+            darkMode={darkMode}
+            href={process.env.NEXT_PUBLIC_LEGAL_SLUG}
+          >
+            Privacy Policy
+          </LegalLink>
+        </LegalColumn>
+        <LegalColumn>
+          <LegalLink darkMode={darkMode} href="/terms-of-service">
+            Terms of Service
+          </LegalLink>
+        </LegalColumn>
+      </LegalRow>
+    </LegalContainer>
+  );
 };
