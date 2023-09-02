25420495dd4caac4b9df8bc60c745ee6785e721a
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-09-02 02:13:15 -0400

    cleanup & move Login & ResetPassword components, add new root routes

--- components/NotifyForm/NotifyForm.tsx
+++ components/NotifyForm/NotifyForm.tsx
@@ -152,7 +152,7 @@ export const NotifyForm = () => {
     // setFirstName('');
     // setLastName('');
   };
-  
+
   useEffect(() => {
     // if (status === "success") clearFields();
     // if(modalOpen && status === "success") clearFields();
@@ -200,9 +200,7 @@ export const NotifyForm = () => {
           </form>
           {/* MailChimp Status */}
           {status === "sending" && (
-            <NotifyText className="mc__alert mc__alert--sending">
-              sending...
-            </NotifyText>
+            <NotifyText className="mc__alert mc__alert--sending">sending...</NotifyText>
           )}
           {status === "error" && (
             <NotifyText>
