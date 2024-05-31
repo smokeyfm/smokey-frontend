a79e2834de12bfab160b648673959f34f7ca8de0
Aaron Smulktis <aaronsmulktis@gmail.com>
2024-05-30 23:45:51 -0700

    missing env var

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,6 +64,26 @@ class MyDocument extends Document {
       }]
     `;
 
+    const FacebookPixelObject = `
+      <!-- Meta Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '${tracking.FB_PIXEL_ID}');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=${tracking.FB_PIXEL_ID}&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Meta Pixel Code -->
+    `;
+
     return (
       <Html>
         <Head>
@@ -149,6 +169,8 @@ class MyDocument extends Document {
               `
             }}
           />
+          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUqxah2mT_0iaosOBBSIKRy0lw7f6wdLA&libraries=places" />
+          <script dangerouslySetInnerHTML={{ __html: FacebookPixelObject }} />
         </Head>
         <body>
           <Main />
